36b6c63e452b44a9afe5b1003b4ad0642e1dfb67
Grenui92 <grenui92@gmail.com>
2023-06-26 23:22:51 +0300

    reset password

--- my_cards/users/urls.py
+++ my_cards/users/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from . import views
+from .views import MyRegistrationView, MyLoginView, MyLogoutView, MyResetPasswordView
+from django.contrib.auth.views import  PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.user_signup, name='signup'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('signup/', MyRegistrationView.as_view(), name='signup'),
+    path('login/', MyLoginView.as_view(), name='login'),
+    path('logout/', MyLogoutView.as_view(), name='logout'),
+    path('password_reset/', MyResetPasswordView.as_view(), name='password_reset'),
+    path('reset-password/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset-password/confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',
+                                          success_url='/users/reset-password/complete/'),
+         name='password_reset_confirm'),
+    path('reset-password/complete/',
+         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
