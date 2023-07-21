4c9af66caaf3f4d08192ad8d692ce5b5a9c6c7d9
grenui92 <grenui92@gmail.com>
2023-07-21 17:44:11 +0300

    2

--- my_cards/times/views.py
+++ my_cards/times/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views import View
 
+from times.models import Sentences, Times
+
 class TimesView(View):
     template_name = 'times/times.html'
 
     def get(self, request):
+        times = Times.objects.all()
+        result = []
+        for time in times:
+            result.append(Sentences.objects.filter(time=time.id, owner=request.user.id))
         return render(request, self.template_name)
