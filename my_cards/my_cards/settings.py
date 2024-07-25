b5318410d172c8aa7f1c0d1ed28a3cd455e44e19
grenui92@gmail.com <grenui92@gmail.com>
2024-07-25 20:37:05 +0300

    redis

--- my_cards/my_cards/settings.py
+++ my_cards/my_cards/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
         'PORT': config('POSTGRES_PORT')
     }
 }
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/1"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
