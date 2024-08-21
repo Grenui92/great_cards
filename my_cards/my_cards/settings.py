c8351e0ebdc667e6619b9030dd732746cf5c24bf
grenui92@gmail.com <grenui92@gmail.com>
2024-08-21 08:54:18 +0300

    local

--- my_cards/my_cards/settings.py
+++ my_cards/my_cards/settings.py
@@ -137,6 +137,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -146,3 +147,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 MEDIA_ABSOLUTE_PATH = f'{Path(__file__).absolute().parent.parent}/media'
 
+try:
+    from .settings_local import *
+except ImportError:
+    pass
