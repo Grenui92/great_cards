b460fb329d1d0129307c2b971f57a7898c0a1c23
grenui92@gmail.com <grenui92@gmail.com>
2024-08-21 17:27:37 +0300

    chats

--- my_cards/my_cards/settings.py
+++ my_cards/my_cards/settings.py
@@ -20,14 +20,14 @@ ALLOWED_HOSTS = [
     'www.dontlosehope.website',
 ]
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [("127.0.0.1", 6379)],
-#         },
-#     },
-# }
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 LOGGING = {
     'version': 1,
