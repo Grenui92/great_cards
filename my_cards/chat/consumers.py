f8d9c9594bf43d8e40a85597062a84768bef463d
grenui92@gmail.com <grenui92@gmail.com>
2024-08-22 12:01:45 +0300

    3

--- my_cards/chat/consumers.py
+++ my_cards/chat/consumers.py
@@ -88,8 +88,7 @@ class EnRuTranslateConsumer(WebsocketConsumer):
     def disconnect(self, code):
         pass
 
-    def receive(self, text_data=None, bytes_data=None):
-        logger.info('EnRu')
+    def receive(self, text_data=None, bytes_data=None, *args, **kwargs):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
