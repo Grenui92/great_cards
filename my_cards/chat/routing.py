afc7ab85253a35f61d23dc6fc63109ea5ab39b8f
grenui92@gmail.com <grenui92@gmail.com>
2024-08-22 09:15:22 +0300

    ws/

--- my_cards/chat/routing.py
+++ my_cards/chat/routing.py
@@ -4,5 +4,7 @@ websocket_urlpatterns = [
     re_path('chat_ai/', ChatConsumer.as_asgi()),
     re_path('text_correction/', CorrectorConsumer.as_asgi()),
     re_path('translate_to_en/', RuEnTranslateConsumer.as_asgi()),
-    re_path('translate_to_ru', EnRuTranslateConsumer.as_asgi())
+    re_path('translate_to_ru', EnRuTranslateConsumer.as_asgi()),
+    re_path('ws/translate_to_en/', RuEnTranslateConsumer.as_asgi()),
+    re_path('ws/translate_to_ru/', EnRuTranslateConsumer.as_asgi())
 ]
