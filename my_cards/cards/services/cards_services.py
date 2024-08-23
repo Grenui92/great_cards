c2cc4b0a9540385dbcfa424d0ba5d08033aa7766
grenui92@gmail.com <grenui92@gmail.com>
2024-08-23 19:22:07 +0300

    delete docs files

--- my_cards/cards/services/cards_services.py
+++ my_cards/cards/services/cards_services.py
@@ -10,16 +10,11 @@ class CardsServices:
 
     @classmethod
     def get_information_from_forms(cls, request):
-        """
-        The get_information_from_forms function is a helper function that
-        returns the information from the form.
-        It takes in one argument, which is a form object.
-        It then returns four attributes: english_word, russian_word,
-        word_usage and collection.
+        """function get_information_from_forms takes in a request object and
+        returns the values of the form fields.
 
-        :param cls: Get the class of the object
-        :param form: Get the data from the form
-        :return: The following:
+        :param cls: Refer to the class itself
+        :return: A tuple of four values
         """
 
         english_word = request.GET['english_word']
@@ -31,19 +26,19 @@ class CardsServices:
 
     @classmethod
     def get_card_data(cls, english_word, russian_word, word_usage):
-        """
-        The generate_data function takes in three arguments: english_word,
+        """The generate_data function takes in three arguments: english_word,
         russian_word, and word_usage.
         If the user does not provide an English word or a Russian word,
-        the function will use Google Translate to translate the provided
+        the function will use translator() to translate the provided
         language into the other language.
         If no usage is provided by the user, then it will be generated using
         the fact generator function.
 
+        :param cls: Refer to the class itself
         :param english_word: Pass the english word to the function
         :param russian_word: Pass the russian word to the function
         :param word_usage: Pass a fact about the word to the function
-        :return: A tuple of three values
+        :return: A tuple of three values.
         """
 
         try:
@@ -70,17 +65,17 @@ class CardsServices:
                                  english=None,
                                  russian=None,
                                  card_id=None):
-        """
-        The get_card_from_collection function takes in a string of either
+        """The get_card_from_collection function takes in a string of either
         an English word or Russian word, and returns the corresponding card
         object from the database.
         If no such card exists, it raises a ValueError.
 
-        :param cls: Specify the model class that is being queried
-        :param english: Get the card from the database using its english word
-        :param russian: Get the card from the database
-        :param card_id: Get a card by its id
-        :return: A card from the database
+        :param cls: Refer to the class itself
+        :param english: Get the card from the database using its english word,
+        default is None
+        :param russian: Get the card from the database, default is None
+        :param card_id: Get a card by its id, default is None
+        :return: A Card instance from the database
         """
         if english:
             card = Cards.objects.get(english_word=english)
@@ -95,8 +90,7 @@ class CardsServices:
 
     @classmethod
     def get_new_card(cls, english, russian, usage, collection):
-        """
-        The get_new_card function takes a form and returns a message.
+        """The get_new_card function takes a form and returns a message.
 
         :param cls: Refer to the class itself
         :param form: Get the information from the form
@@ -104,7 +98,6 @@ class CardsServices:
         """
 
         try:
-
             card = cls.get_card_from_collection(
                 english=english, russian=russian)
             collection.cards.add(card)
--- my_cards/users/apps.py
+++ my_cards/users/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """UsersConfig is a class for user configuration."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
