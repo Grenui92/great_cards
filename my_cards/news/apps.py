48859345ae87559d84cdac04be2d7deadd52c05d
grenui92@gmail.com <grenui92@gmail.com>
2024-08-23 22:05:56 +0300

    news docstrings

--- my_cards/news/apps.py
+++ my_cards/news/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class NewsConfig(AppConfig):
+    """News app config."""
     default_auto_field = "django.db.models.BigAutoField"
     name = "news"
--- my_cards/news/views.py
+++ my_cards/news/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render
 
 
 class NewsListView(View):
+    """News list view."""
+
     template_name = 'news/index.html'
 
     def get(self, request):
-        video = 'Hi, my dear friends!'
-        return render(request, self.template_name, context={'video': video})
+        """Return the news list page.
+
+        :param request: request object
+        :return: render
+        """
+        message = 'Hi, my dear friends!'
+        return render(request, self.template_name, context={'video': message})
