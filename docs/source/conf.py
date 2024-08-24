8ce275f07f8b28b7e9f5fe167779a9f11b057106
grenui92@gmail.com <grenui92@gmail.com>
2024-08-24 15:42:58 +0300

    new static path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ exclude_patterns = [
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'python_docs_theme'
-html_static_path = ['_static']
+html_static_path = ['static']
 html_css_files = [
     'custom.css',
 ]
